b4303541a3576e3fcfd36446f615c95533d010d8
LauTB <45464137+LauTB@users.noreply.github.com>
2021-12-09 16:08:57 -0500

    fixed move_code comments

--- inputs.pl
+++ inputs.pl
@@ -1,4 +1,9 @@
-get_move(L):- read(X), tokenize(X,L).
+:-module('inputs', [get_move/1, describe_piece/4, get_pieces/2, move_code/2]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   OBTENER LA ENTRADA Y PROCESARLA  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%obtener la jugada del player
+get_play(L):- read(X), tokenize(X,L).
 
 %separa el movimiento del jugador para que quede de la forma:
 %[Pieza1, Punto de contacto, Pieza2]
@@ -10,17 +15,21 @@ get_pieces(L, P1, P2):- nth0(0, L, P1), nth0(2, L, P2).
 %dada una pieza(S) obtiene el color(C), tipo de pieza(P) y el id(N) 
 describe_piece(S,C,T,N):- 
     string_chars(S, Ch), 
-    nth0(0, Ch, C),
+    nth0(0, Ch, C1),%negro = 0, blanco = 1
     nth0(1, Ch, T),
     nth0(2, Ch, N).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   CONVERTIR LOS DATOS A INT   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %convertir el string de movimiento en un código de movimiento
-move_code("|*", 1).%izquierda
-move_code("/*", 2).%izquierda abajo
-move_code("*\", 3).%derecha abajo
-move_code("*|", 4).%derecha
-move_code("*/", 5).%derecha arriba
-move_code("\*", 6).%izquierda arriba
+move_code("|*", 1).% derecha
+move_code("/*", 2).%derecha abajo
+move_code("*\", 3).%izquierda abajo
+move_code("*|", 4).%izquierda
+move_code("*/", 5).%izquierda arriba
+move_code("\*", 6).%derecha arriba
 
 
 
